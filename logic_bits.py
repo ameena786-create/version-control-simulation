6a808b609c1e42004994ca2ad4dfe2391f75b3f4
ameena786-create <khammaru26@gmail.com>
2026-06-09 14:47:41 -0700

    Added 3 labs

--- logic_bits.py
+++ logic_bits.py
@@ -4,10 +4,10 @@ b = False
 print("AND:", a and b)
 print("OR:", a or b)
 print("NOT:", not a)
+print ("NOT", not b)
 
 x = 5
 y = 3
-
 print("x & y:", bin(x & y))
 print("x | y:", bin(x | y))
 print("x ^ y:", bin(x ^ y))
